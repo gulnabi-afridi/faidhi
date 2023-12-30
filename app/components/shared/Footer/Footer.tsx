f619e6244dcd079a80255be692f60823b5584f1c
gulnabi-afridi <109666507+gulnabi-afridi@users.noreply.github.com>
2023-12-30 08:56:15 +0500

    footer structure set

--- app/components/shared/Footer/Footer.tsx
+++ app/components/shared/Footer/Footer.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className='w-full h-[180px] bg-red-main flex justify-center items-center text-white-light text-[30px] capitalize font-semibold'>
+      Footer
+    </div>
+  );
 };
 
 export default Footer;
