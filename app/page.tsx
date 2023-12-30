d07af9c7ea0690e13f7ee32a1f5bac6594832f82
gulnabi-afridi <109666507+gulnabi-afridi@users.noreply.github.com>
2023-12-30 10:44:42 +0500

    section 4 developed

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from './components/Home/Hero';
 import Section2 from './components/Home/Section2';
 import Section3 from './components/Home/Section3';
+import Section4 from './components/Home/Section4';
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <Section2 />
       <Section3 />
+      <Section4 />
     </main>
   );
 }
