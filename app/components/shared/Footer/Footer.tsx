5b644d33bc81e511de2ad1eb5da6d1ef2c8a0db4
gulnabi-afridi <109666507+gulnabi-afridi@users.noreply.github.com>
2024-01-03 03:30:15 +0500

    footer updated

--- app/components/shared/Footer/Footer.tsx
+++ app/components/shared/Footer/Footer.tsx
@@ -4,21 +4,20 @@ import Link from 'next/link';
 
 const Footer = () => {
   return (
-    <div className='w-full min-h-[220px] md:min-h-[280px] sm:pt-0 pt-10 bg-[#efece8] grid grid-cols-1 sm:grid-cols-2 justify-center items-center mt-16'>
+    <div className='w-full min-h-[120px] sm:min-h-[120px] md:h-[150px] lg:min-h-[180px] sm:pt-0 pt-8 bg-[#efece8] flex sm:flex-row flex-col gap-0 sm:gap-8 justify-center items-center mt-8 sm:mt-16'>
       {logos.map((item, index) => {
         return (
-          <div key={index} className='w-full flex justify-center items-center'>
-            <Link
-              href={item.link}
-              className={`w-full hover:opacity-80 ${
-                index === 1
-                  ? 'max-w-[200px] sm:max-w-[240px] md:max-w-[300px] h-[140px] sm:h-[170px] md:h-[200px]'
-                  : 'max-w-[100px] sm:max-w-[150px] md:max-w-[180px] h-[80px] md:h-[130px]'
-              }  relative`}
-            >
-              <Image src={item.src} fill alt='' className='object-fill' />
-            </Link>
-          </div>
+          <Link
+            key={index}
+            href={item.link}
+            className={` hover:opacity-80 ${
+              index === 1
+                ? 'w-[120px] sm:w-[150px] md:w-[230px] h-[70px] sm:h-[100px] md:h-[120px]'
+                : 'w-[50px] sm:w-[50px] md:w-[70px] h-[50px] md:h-[80px]'
+            }  relative`}
+          >
+            <Image src={item.src} fill alt='' className='object-fill' />
+          </Link>
         );
       })}
     </div>
