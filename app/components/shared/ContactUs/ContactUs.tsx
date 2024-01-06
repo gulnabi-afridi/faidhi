b1af71c30090d3765e11d72be70bfd60804ee0f4
gulnabi-afridi <109666507+gulnabi-afridi@users.noreply.github.com>
2024-01-06 23:32:19 +0500

    footer changed

--- app/components/shared/ContactUs/ContactUs.tsx
+++ app/components/shared/ContactUs/ContactUs.tsx
@@ -1,16 +1,17 @@
 'use client';
 
+import Link from 'next/link';
 import React from 'react';
-import { Fade, Slide } from 'react-awesome-reveal';
+import { Fade, Slide, Zoom } from 'react-awesome-reveal';
 
 const ContactUs = () => {
   return (
-    <div className='w-full h-[220px] sm:h-[350px] md:h-[400px] bg-[url(/assets/contactUs.webp)] bg-cover flex flex-col justify-center items-center bg-no-repeat relative bg-center'>
+    <div className='w-full h-[300px] sm:h-[350px] md:h-[400px] bg-[url(/assets/contactUs.webp)] bg-cover flex flex-col justify-center items-center bg-no-repeat relative bg-center'>
       <div className='absolute inset-0 bg-black bg-opacity-30'></div>
       <Fade duration={1000} triggerOnce>
-        <div className='flex flex-col gap-0 md:gap-0 relative w-full justify-center items-center px-8'>
+        <div className='flex flex-col gap-1 sm:gap-6 relative w-full justify-center items-center px-8'>
           <Slide direction='down' duration={1000} triggerOnce>
-            <h1 className='text-[35px] sm:text-[50px] text-white-main font-bold tracking-wider opacity-80'>
+            <h1 className='text-[35px] text-center leading-[40px] sm:text-[50px] text-white-main font-bold tracking-wider opacity-80'>
               Connect With Us
             </h1>
           </Slide>
@@ -20,6 +21,21 @@ const ContactUs = () => {
               collaborate with, so go on and get in touch with us!
             </p>
           </Slide>
+
+          <Zoom
+            duration={1000}
+            delay={1000}
+            triggerOnce
+            className='mt-6 sm:mt-3'
+          >
+            <Link
+              href='https://honour.sg/initiatives/'
+              target='blank'
+              className='px-6 py-3 rounded-[60px] tracking-wider hover:opacity-80 active:translate-y-[2px] text-white-main bg-red-main font-medium text-[18px]'
+            >
+              Connect Us
+            </Link>
+          </Zoom>
         </div>
       </Fade>
     </div>
--- app/components/shared/Footer/Footer.tsx
+++ app/components/shared/Footer/Footer.tsx
@@ -3,40 +3,84 @@
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import { Fade, Zoom } from 'react-awesome-reveal';
+import { Fade, Zoom, Slide } from 'react-awesome-reveal';
+import Wrapper from '../ComponentWrapper/ComponentWrapper';
 
 const Footer = () => {
   return (
-    <div className='w-full min-h-[120px] sm:min-h-[120px] md:h-[150px] lg:min-h-[180px] sm:pt-0 pt-4 bg-[#efece8] flex sm:flex-row flex-col gap-0 sm:gap-8 justify-center items-center '>
-      {logos.map((item, index) => {
-        return (
-          <Link
-            key={index}
-            target='/blank'
-            href={item.link}
-            className={` hover:opacity-80 ${
-              index === 1
-                ? 'w-[120px] sm:w-[150px] md:w-[240px] h-[70px] sm:h-[100px] md:h-[130px]'
-                : 'w-[50px] sm:w-[50px] md:w-[70px] h-[50px] md:h-[80px]'
-            }  relative`}
-          >
-            <Image src={item.src} fill alt='' className='object-fill' />
-          </Link>
-        );
-      })}
+    <div className='w-full py-8 sm:py-12 bg-black'>
+      <Wrapper style=' h-full'>
+        <Fade duration={1000} triggerOnce>
+          <div className='w-full h-full flex gap-[5px] sm:gap-[7px] flex-col'>
+            {footerData.map((item, index) => (
+              <Slide
+                direction='up'
+                duration={1000}
+                delay={index * 100}
+                triggerOnce
+                key={index}
+              >
+                {index + 1 === footerData.length ? (
+                  <a
+                    href={`mailto:${item}`}
+                    target='_blank'
+                    rel='noreferrer'
+                    className='text-[14px] sm:text-[17px] text-white-main font-semibold'
+                  >
+                    Email: <span className='opacity-80'>{item}</span>
+                  </a>
+                ) : (
+                  <p className='text-[14px] sm:text-[17px] text-white-main font-semibold'>
+                    {item}
+                  </p>
+                )}
+              </Slide>
+            ))}
+          </div>
+        </Fade>
+      </Wrapper>
     </div>
   );
 };
 
-const logos = [
-  {
-    link: 'https://honour.sg/',
-    src: '/assets/logo.png',
-  },
-  {
-    link: 'https://www.nhb.gov.sg/',
-    src: '/assets/logo2.png',
-  },
+export default Footer;
+
+const footerData = [
+  'Honour (Singapore) Limited',
+  '6 Shenton Way, OUE Downtown 2, #24-11',
+  'Singapore 068809',
+  'Telephone: +65 6438 8175',
+  'admin@honour.sg',
 ];
 
-export default Footer;
+{
+  /* <div className='w-full min-h-[120px] sm:min-h-[120px] md:h-[150px] lg:min-h-[180px] sm:pt-0 pt-4 bg-[#efece8] flex sm:flex-row flex-col gap-0 sm:gap-8 justify-center items-center '>
+{logos.map((item, index) => {
+  return (
+    <Link
+      key={index}
+      target='/blank'
+      href={item.link}
+      className={` hover:opacity-80 ${
+        index === 1
+          ? 'w-[120px] sm:w-[150px] md:w-[240px] h-[70px] sm:h-[100px] md:h-[130px]'
+          : 'w-[50px] sm:w-[50px] md:w-[70px] h-[50px] md:h-[80px]'
+      }  relative`}
+    >
+      <Image src={item.src} fill alt='' className='object-fill' />
+    </Link>
+  );
+})}
+</div> */
+}
+
+// const logos = [
+//   {
+//     link: 'https://honour.sg/',
+//     src: '/assets/logo.png',
+//   },
+//   {
+//     link: 'https://www.nhb.gov.sg/',
+//     src: '/assets/logo2.png',
+//   },
+// ];
